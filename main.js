1e492afe4e3ae711a9ef229bd862d2ff3fb87601
Malik Shahazad <m.shahzad.mail.56@gmail.com>
2023-07-27 14:42:19 +0500

    age calculator

---
+++ main.js
@@ -0,0 +1,27 @@
+function calculate(){
+    setInterval(()=>{
+        let birthdate=new Date(document.getElementById('birthday').value);
+
+        let now = new Date();
+        let ageInMs = now.getTime() -birthdate.getTime();
+        
+        let ageInS =ageInMs / 1000;
+        let ageInMins =ageInS / 60;
+        let ageInHrs =ageInMins / 60;
+        let ageInDays =ageInHrs / 24;
+        let ageInMonths =ageInDays / 30;
+        let ageInyears =ageInMonths / 12;
+
+        document.querySelector('#years').innerHTML =Math.floor(ageInyears);
+        document.querySelector('#months').innerHTML= Math.floor(ageInMonths % 12);
+        document.querySelector('#days').innerHTML = Math.floor(ageInDays % 30.4375);
+        document.querySelector('#hours').innerHTML= Math.floor(ageInHrs %24);
+        document.querySelector('#minutes').innerHTML= Math.floor(ageInMins %60);
+        document.querySelector('#seconds').innerHTML= Math.floor(ageInS %60);
+        document.querySelector('#second').style.borderBottom = '1px grey solid';
+    }, 1000);
+}
+
+function reset (){
+    window.location.reload();
+}
